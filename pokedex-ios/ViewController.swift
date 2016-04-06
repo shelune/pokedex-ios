8574b20f105098d73db44e559bc834427cd08bbd
Your Name <you@example.com>
2016-04-06 15:30:08 +0300

    Initial Commit

---
+++ pokedex-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-ios
+//
+//  Created by iosdev on 6.4.2016.
+//  Copyright © 2016 iosdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
