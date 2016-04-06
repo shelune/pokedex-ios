7c0ab3529b44dae122d1ecf73a42d4844283716c
Your Name <you@example.com>
2016-04-06 18:24:04 +0300

    add collection dex view

---
+++ pokedex-ios/iBeamon.swift
@@ -0,0 +1,27 @@
+//
+//  iBeamon.swift
+//  pokedex-ios
+//
+//  Created by iosdev on 6.4.2016.
+//  Copyright © 2016 iosdev. All rights reserved.
+//
+
+import Foundation
+
+class iBeamon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        _name = name
+        _pokedexId = pokedexId
+    }
+}
