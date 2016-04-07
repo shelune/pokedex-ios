297d682fc821f5f635521b27e27a0d718a40eb55
Your Name <you@example.com>
2016-04-07 15:32:31 +0300

    Music added, can be turned on / off Also searching works

--- pokedex-ios/ViewController.swift
+++ pokedex-ios/ViewController.swift
@@ -8,23 +8,32 @@
 
 import UIKit
 import CoreData
+import AVFoundation
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource, UISearchBarDelegate {
 
     @IBOutlet weak var collectionDex: UICollectionView!
     @IBOutlet weak var collectionTeam: UICollectionView!
     
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     var pokemons = [NSManagedObject]()
+    var filteredPokemons = [NSManagedObject]()
+    var musicPlayer: AVAudioPlayer!
+    var inSearchMode = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // setting up delegation & data source for 2 collections
+        // setting up delegation & data source
         collectionDex.delegate = self
         collectionTeam.delegate = self
         
         collectionDex.dataSource = self
         collectionTeam.dataSource = self
+        
+        searchBar.delegate = self
+        searchBar.returnKeyType = UIReturnKeyType.Done
         
         // reset transparent background for collection view
         collectionDex.backgroundColor = UIColor.clearColor()
@@ -32,6 +41,21 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
         // parsing data from csv
         parsePokemonCSV()
+        
+        // music
+        initAudio()
+    }
+    
+    func initAudio() {
+        let path = NSBundle.mainBundle().pathForResource("main-theme", ofType: "mp3")
+        do {
+            musicPlayer = try AVAudioPlayer(contentsOfURL: NSURL(string: path!)!)
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
+        } catch let err as NSError {
+            print("Error with Audio?")
+        }
     }
     
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
@@ -41,7 +65,13 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell {
             
-            let pokemon = pokemons[indexPath.row]
+            let pokemon: NSManagedObject!
+            
+            if inSearchMode {
+                pokemon = filteredPokemons[indexPath.row]
+            } else {
+                pokemon = pokemons[indexPath.row]
+            }
             cell.configureCell(pokemon)
             return cell
         } else {
@@ -51,10 +81,12 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if (collectionView.tag == 1) {
+            if inSearchMode {
+                return filteredPokemons.count
+            }
             return pokemons.count
-        } else {
-            return 3
         }
+        return 3
     }
     
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
@@ -66,6 +98,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     // Parsing
+    
     
     func parsePokemonCSV() {
         let path = NSBundle.mainBundle().pathForResource("pokemon", ofType: "csv")!
@@ -84,10 +117,11 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
                 poke.setValue(name, forKey: "name")
                 poke.setValue(pokeId, forKey: "pokedexId")
                 
+                
                 pokemons.append(poke)
             }
         } catch {
-            
+            print("Error with parsing CSV?")
         }
     }
     
@@ -113,6 +147,38 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             print("Could not fetch \(error), \(error.userInfo)")
         }
          */
+    }
+    
+    // filter search phrase
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text == nil || (searchBar.text?.isEmpty)! {
+            inSearchMode = false
+            view.endEditing(true)
+            collectionDex.reloadData()
+        } else {
+            inSearchMode = true
+            let searchPhrase = searchBar.text!.lowercaseString
+            filteredPokemons = pokemons.filter({
+                ($0.valueForKey("name") as! String).lowercaseString.rangeOfString(searchPhrase) != nil
+            })
+            print(filteredPokemons)
+            collectionDex.reloadData()
+        }
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+    
+    // turn on / off music
+    @IBAction func musicBtnPressed(sender: UIButton) {
+        if musicPlayer.playing {
+            musicPlayer.stop()
+            sender.alpha = 0.2
+        } else {
+            musicPlayer.play()
+            sender.alpha = 1.0
+        }
     }
 }
 
