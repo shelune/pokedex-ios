3ca39de6ea8106e10aadd0c26a9bf803f9d95e9b
Your Name <you@example.com>
2016-04-08 11:27:48 +0300

    merging with hasse #1, not working now

---
+++ pokedex-ios/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  pokedex-ios
+//
+//  Created by iosdev on 8.4.2016.
+//  Copyright © 2016 iosdev. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class User: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
